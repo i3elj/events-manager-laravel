ebd1fe5b5f5350f9bc0f24fefd32b309560c51d9
i3elj <guisande.bernardo@gmail.com>
2022-02-04 14:29:37 -0300

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,8 @@
 @section('title', 'HDC Eventns')
 
 @section('content')
-    <h1>Titulo</h1>
-    <h2>Perfil de {{ $nome }}</h2>
-    {{-- Comentarios em Blade --}}
-    <ul>
-        <li>Nome: {{ $nome }}</li>
-        <li>Idade: {{ $idade }} anos</li>
-        <li>Altura: {{ $altura }}</li>
-    </ul>
+
+@foreach ($events as $event)
+    <p>{{ $event->title }} -- {{ $event->description }}</p>
+@endforeach
 @endsection
