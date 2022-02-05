c2b9b7b1a4b9db4123d697ef489e94e7eae517e4
i3elj <guisande.bernardo@gmail.com>
2022-02-05 18:24:20 -0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 use App\Http\Controllers\EventController;
 
-Route::get('/',                 [EventController::class, 'index']);
+Route::get ('/',                 [EventController::class, 'index']);
+Route::get ('/events/create',    [EventController::class, 'create']);
 Route::post('/events',           [EventController::class, 'store']);
-Route::get('/events/create',    [EventController::class, 'create']);
