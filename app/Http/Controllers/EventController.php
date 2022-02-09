109b2daeda7a8c88b5765b3eebc5bb5cf7481f3a
i3elj <guisande.bernardo@gmail.com>
2022-02-09 11:09:54 -0300

    commit

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -9,8 +9,12 @@ class EventController extends Controller
 {
 
     public function index() {
-        $events = Event::all();
-        return view( 'welcome', ['events' => $events]);
+        $search = request('search');
+
+        if  ($search) { $events = Event::where([ ['title', 'like', '%'.$search.'%' ] ])->get(); } 
+        else          { $events = Event::all(); }
+
+        return view( 'welcome', [ 'events' => $events, 'search' => $search ]);
     }
 
     public function create() {
@@ -21,6 +25,7 @@ class EventController extends Controller
         $event = new Event;
 
         $event->title = $request->title;
+        $event->date = $request->date;
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
